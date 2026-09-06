7f569457ae0c1a52c224e04ff8882e95847881e8
Zait Paulo <darknessw580@gmail.com>
2026-09-06 17:27:10 -0500

    feat: (storefront) route group, layout with brand font and css variables

---
+++ src/app/(storefront)/layout.tsx
@@ -0,0 +1,42 @@
+import type { ReactNode } from 'react'
+import { Barlow, Barlow_Condensed } from 'next/font/google'
+import { getShowcase } from '@/lib/showcase/data'
+import { resolveBrandColor } from '@/lib/showcase/format'
+
+const barlow = Barlow({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-barlow',
+  display: 'swap',
+})
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ['latin'],
+  weight: ['600', '700', '800'],
+  style: ['normal', 'italic'],
+  variable: '--font-barlow-condensed',
+  display: 'swap',
+})
+
+export default async function StorefrontLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  const data = await getShowcase()
+  const brandColor = resolveBrandColor(data?.account?.public_brand_color)
+
+  return (
+    <div
+      className={`${barlow.variable} ${barlowCondensed.variable} min-h-screen bg-[#f7f9fb] text-[#191c1e] antialiased`}
+      style={
+        {
+          '--brand': brandColor,
+          fontFamily: 'var(--font-barlow), sans-serif',
+        } as React.CSSProperties
+      }
+    >
+      {children}
+    </div>
+  )
+}
